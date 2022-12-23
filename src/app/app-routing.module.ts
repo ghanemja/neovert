72d4bc2b0f15dba631ec113ceb8502cfbfd0b090
ghanemja <janghanem@gmail.com>
2022-12-23 17:14:09 -0500

    stepper and initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ConsoleComponent } from './console/console.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'ad-console', component: ConsoleComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ConsoleComponent } from './console/console.component';
+import { StepperComponent } from './stepper/stepper.component';
+import { CdkStepperModule } from '@angular/cdk/stepper';
+import { IgxGridModule } from 'igniteui-angular';
+import { 
+	IgxStepperModule,
+	IgxMaskModule,
+	IgxInputGroupModule,
+	IgxButtonModule,
+	IgxRadioModule,
+	IgxCardModule,
+	IgxCheckboxModule,
+	IgxSelectModule,
+	IgxIconModule,
+	IgxBadgeModule
+ } from "igniteui-angular";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    ConsoleComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    CdkStepperModule,
+    IgxStepperModule,
+    IgxMaskModule,
+    IgxInputGroupModule,
+    IgxButtonModule,
+    IgxRadioModule,
+    IgxCardModule,
+    IgxCheckboxModule,
+    IgxSelectModule,
+    IgxIconModule,
+    IgxBadgeModule,
+    IgxGridModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
